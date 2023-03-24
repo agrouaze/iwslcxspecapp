72a12e4c3e2a41b71274bf1b8fb94cade50ad75f
agrouaze <antoine.grouazel@gmail.com>
2023-03-24 23:45:07 +0100

    fix bugs to have a working app with only xspecs

--- iwslcxspecapp/iw_slc_xspec_visu_panel_app.py
+++ iwslcxspecapp/iw_slc_xspec_visu_panel_app.py
@@ -18,9 +18,9 @@ import cartopy.crs as ccrs
 import geoviews as gv
 import xsar
 import sys
-import get_path_from_base_SAFE
-import match_GRD_SLC
-from symmetrize_xspec import symmetrize_xspectrum
+#import iwslcxspecapp.get_path_from_base_SAFE
+#import iwslcxspecapp.match_GRD_SLC
+from iwslcxspecapp.symmetrize_xspec import symmetrize_xspectrum
 import os
 import glob
 from holoviews.operation.datashader import datashade, rasterize
@@ -31,30 +31,13 @@ from bokeh.models import (CDSView, ColorBar, ColumnDataSource,
                           GeoJSONDataSource, HoverTool,
                           LinearColorMapper, Slider)
 from bokeh.plotting import figure
-import yaml
-# Read YAML file
-src_dir = os.path.dirname(__file__)
-if os.path.exists(os.path.join(src_dir,"localconfig.yml")):
-    config_path = os.path.join(src_dir,"localconfig.yml")
-else:
-    config_path = os.path.join(src_dir,"config.yml")
-print('config_path',config_path)
-with open(config_path, 'r') as stream:
-    data_loaded = yaml.safe_load(stream)
-# L1B_file_default = "/home1/scratch/agrouaze/l1b/S1A_IW_SLC__1SDV_20180104T061957_20180104T062025_020001_02211F_77E6.SAFE_L1B_xspec_IFR_VV_0.1.nc"
-# L1B_file_default = '/home1/scratch/agrouaze/l1b/S1A_IW_SLC__1SDV_20210413T043041_20210413T043109_037427_04695F_FE5C.SAFE_L1B_xspec_IFR_0.3.nc'
-# L1B_file_default = '/home1/scratch/agrouaze/l1b/S1A_IW_SLC__1SDV_20220921T063225_20220921T063252_045099_05639C_D284.SAFE/s1a-iw2-slc-vv-20220921t063225-20220921t063251-045099-05639c-005_L1B_xspec_IFR_0.6.nc'
-# L1B_file_default = '/home1/scratch/agrouaze/l1b/S1A_IW_SLC__1SDV_20220921T063225_20220921T063252_045099_05639C_D284.SAFE/s1a-iw2-slc-vv-20220921t063225-20220921t063251-045099-05639c-005_L1B_xspec_IFR_0.6.nc'
-# L1B_file_default = '/home1/scratch/agrouaze/l1b/S1A_IW_SLC__1SDV_20170907T103019_20170907T103047_018268_01EB76_5F55.SAFE/s1a-iw2-slc-vv-20170907t103020-20170907t103045-018268-01eb76-005_L1B_xspec_IFR_0.6.nc'
-# subswath = 'subswath_1'
-#subswath = 'iw1_vv'
-L1B_file_default = data_loaded['datadir']
-files_dir = data_loaded['files_dir']
-print('L1B_file_default',L1B_file_default)
+
 main_map_width = 600
 main_map_height = 500
-
-
+small_plot_height = 300
+small_plot_width = 400
+checkbox_burst = pn.widgets.Select(options=['intra', 'inter'], name='burst Type')
+checkbox_subswath = pn.widgets.Select(options=['iw1_vv', 'iw2_vv', 'iw3_vv', 'iw1_vh'], name='subswath #')
 ################################################
 def get_tile_corner_image_idx(ds, bursti, tile_line_i, tile_sample_i):
     """
@@ -221,6 +204,7 @@ def display_xspec_cart_holo(ds, bu=0, li=0, sam=0, typee='Re'):
     set_xspec = set_xspec.swap_dims({'freq_line': 'k_az', 'freq_sample': 'k_rg'})
     print('symmetrize_xspectrum')
     set_xspec = symmetrize_xspectrum(set_xspec, dim_range='k_rg', dim_azimuth='k_az')
+    print('symmetrzied')
     kx_varname = 'k_rg'
     ky_varname = 'k_az'
     #sp = set_xspec['xspectra_2tau_%s' % typee].mean(dim='2tau')
@@ -229,7 +213,7 @@ def display_xspec_cart_holo(ds, bu=0, li=0, sam=0, typee='Re'):
     print('spectra is ready')
     hv.extension('bokeh')
     if typee == 'Re':
-        im = hv.Image(abs(sp2.real), kdims=[kx_varname, ky_varname]).opts(width=small_plot_with,
+        im = hv.Image(abs(sp2.real), kdims=[kx_varname, ky_varname]).opts(width=small_plot_width,
                                                                      height=small_plot_height,
                                                                      cmap=cmap, colorbar=True, xlim=(
                 -0.07, 0.07))  # gist_ncar_r , 'cet_linear_wyor_100_45_c55'
@@ -239,7 +223,7 @@ def display_xspec_cart_holo(ds, bu=0, li=0, sam=0, typee='Re'):
         else:
             extrema = float(abs(sp2.imag).max().values)
 
-        im = hv.Image(sp2, kdims=[kx_varname, ky_varname]).opts(width=small_plot_with,
+        im = hv.Image(sp2.imag, kdims=[kx_varname, ky_varname]).opts(width=small_plot_width,
                                                                 height=small_plot_height,
                                                                 cmap=cmap, colorbar=True,
                                                                 xlim=(-0.07, 0.07),
@@ -415,14 +399,15 @@ class monAppIW_SLC:
         res = gv.Path(self.xsarobjgrd.footprint)
         return res
 
-    def update_app_burst(self, burst_type, subswath_id, L1B_file=L1B_file_default):
+    def update_app_burst(self, burst_type, subswath_id, L1B_file,all_avail_l1B):
         """
 
         Parameters
         ----------
-        burst_type:  str
-        subswath_id: str
+        burst_type:  str intra or inter
+        subswath_id: str iw1
         L1B_file: str one subswath .nc file (previous version it was one nc file for a SAFE)
+        all_avail_l1B : list
 
         Returns
         -------
@@ -436,7 +421,8 @@ class monAppIW_SLC:
                 L1B_file = L1B_file[0]
                 print('new L1B ', L1B_file)
             else:
-                L1B_file = L1B_file_default  # security util???
+                pass
+                #L1B_file = L1B_file_default  # security util???
         # prepare data
         logging.info('ouai')
 
@@ -563,6 +549,8 @@ class monAppIW_SLC:
         print('creating rows and columns for layout panel/bokeh')
         layout_figures = pn.Row(pn.bind(tap_update_xspec_figures, x=posxy.param.x,
                                         y=posxy.param.y))
+
+        checkbox_files = self.get_checkboxes(all_avail_l1B=all_avail_l1B)
         bokekjap = pn.Row(
             pn.Column(pn.Column(checkbox_files, pn.Row(checkbox_burst, checkbox_subswath), maphandler, posxy)),
             layout_figures,
@@ -576,54 +564,15 @@ class monAppIW_SLC:
         print('return bokeh app layout')
         return bokekjap
 
+    def get_checkboxes(self,all_avail_l1B):
+        ##############################
+        # add the checboxes
+
+        checkbox_files = pn.widgets.Select(options=all_avail_l1B, name='file')
+        ##############################
+        return checkbox_files
+
 
 ############################################################################
 #####################NO MAIN BUT IT LOOKS LIKE#############################
-small_plot_height = 300
-small_plot_with = 400
 
-xsrehandler1 = figure(plot_height=small_plot_height, plot_width=small_plot_with,
-                      tools="pan, wheel_zoom, box_zoom, reset,lasso_select,hover")
-# xsrehandler1 = display_xspec_cart_holo(ds_intra,bu = 3,li = 0,sam = 4,typee='Re')
-xsimhandler1 = figure(plot_height=small_plot_height, plot_width=small_plot_with,
-                      tools="pan, wheel_zoom, box_zoom, reset,lasso_select,hover")
-# xsimhandler1 = display_xspec_cart_holo(ds,bu = 3,li = 0,sam = 4,typee='Im')
-rough_handler1 = figure(plot_height=small_plot_height, plot_width=small_plot_with,
-                        tools="pan, wheel_zoom, box_zoom, reset,lasso_select,hover")
-
-xsrehandler2 = figure(plot_height=small_plot_height, plot_width=small_plot_with,
-                      tools="pan, wheel_zoom, box_zoom, reset,lasso_select,hover")
-# xsrehandler2 = display_xspec_cart_holo(ds,bu = 3,li = 0,sam = 5,typee='Re')
-xsimhandler2 = figure(plot_height=small_plot_height, plot_width=small_plot_with,
-                      tools="pan, wheel_zoom, box_zoom, reset,lasso_select,hover")
-# xsimhandler2 = display_xspec_cart_holo(ds,bu = 3,li = 0,sam = 5,typee='Im')
-rough_handler2 = figure(plot_height=small_plot_height, plot_width=small_plot_with,
-                        tools="pan, wheel_zoom, box_zoom, reset,lasso_select,hover")
-
-# button intra / inter
-checkbox_burst = pn.widgets.Select(options=['intra', 'inter'], name='burst Type')
-checkbox_subswath = pn.widgets.Select(options=['iw1_vv', 'iw2_vv', 'iw3_vv', 'iw1_vh'], name='subswath #')
-streams_select = dict(burst_type=checkbox_burst.param.value)
-
-#files_dir = os.path.abspath(os.path.join(src_dir,'..','assets','S1*','s1*L1B_xspec_IFR*.nc'))
-pattern_list_files_dir = os.path.abspath(os.path.join(files_dir,'S1*','s1*L1B_xspec_IFR*.nc'))
-print('files_dir',files_dir)
-print('pattern_list_files_dir',pattern_list_files_dir)
-all_avail_l1B = sorted(glob.glob(pattern_list_files_dir))
-print('all available L1B', len(all_avail_l1B))
-checkbox_files = pn.widgets.Select(options=all_avail_l1B, name='file')
-# files = pn.widgets.FileSelector('/home1/scratch/agrouaze/l1b/',only_files=True,file_pattern='S1*L1B_xspec_IFR*.nc') # ,value=[L1B_file_default]
-# logging.info('defaut component defined')
-# widget_dmap = hv.DynamicMap(update_app_burst, streams=streams_select)
-# widget_dmap = pn.bind(update_app_burst,burst_type=checkbox.param.value)
-instclass = monAppIW_SLC()
-layout = pn.Row(pn.bind(instclass.update_app_burst,
-                        burst_type=checkbox_burst.param.value,
-                        subswath_id=checkbox_subswath.param.value,
-                        L1B_file=checkbox_files.param.value
-                        ))
-# pn.Row(widget_dmap).servable()
-# layout = pn.Row(xsrehandler1)
-print('go servable')
-layout.servable()
-# pn.panel("# Test").servable()
--- scripts/start_app.bash
+++ scripts/start_app.bash
@@ -3,4 +3,4 @@
 # gogogo
 hosti=`hostname`
 echo 'start panel python app to visualize IW SLC L1B x-spectrum'
-panel serve ../iwslcxspecapp/iwslcxspecapp/iw_slc_xspec_visu_panel_app.py --allow-websocket-origin=$hosti:5007 --allow-websocket-origin=localhost:5007 --port=5007 --autoreload
+panel serve ./start_app.py --allow-websocket-origin=$hosti:5007 --allow-websocket-origin=localhost:5007 --port=5007 --autoreload
